cb528218afd3e02203fafb6dd803c2f96b8df81a
Kevin Frans <kevin@bobthechicken.com>
2017-05-13 17:22:03 -0700

    reimuu

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public Vector2 speed;
+	public float direction;
+
+	private Vector2 direction_vector;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
