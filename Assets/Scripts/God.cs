0b5109eba05847649f8e24551c7b8947774cbeae
Kevin Frans <kevin@bobthechicken.com>
2017-05-21 02:11:33 -0700

    fix

---
+++ Assets/Scripts/God.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class God : MonoBehaviour {
+
+	// Prefab references
+	public Transform player;
+
+	public Transform boss1;
+	public Transform boss2;
+
+	// Use this for initialization
+	void Start () {
+		Transform p = Instantiate(player);
+		p.position = new Vector2(0,0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
