e4455195e3e9bbf52420f196864497562f86da19
Kevin Frans <kevin@bobthechicken.com>
2017-05-21 01:12:20 -0700

    commit

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,20 +1,30 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-	public Vector2 speed;
-	public float direction;
+	public float speed = 1;
+	public float direction = 0;
 
 	private Vector2 direction_vector;
 
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+	{
+		direction_vector = new Vector2(1,0);
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 	}
+
+	public void setDirection(float dir)
+	{
+		direction = dir;
+		direction_vector = new Vector(Mathf.Cos(dir * Mathf.Deg2Rad), Mathf.Sin(dir * Mathf.Deg2Rad))
+	}
+
+
 }
