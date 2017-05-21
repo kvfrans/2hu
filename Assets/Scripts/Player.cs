69616db54bc9a5aab7bfec8fcc82f6564f8e9db2
Daniel Chu <dchu0518@gmail.com>
2017-05-21 02:21:26 -0700

    wip

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour {
 
 	public float speed = 4;
 	public bool shift = false;
+	public int numOfDirections = 0;
 
 	public Vector2 direction_vector;
 
@@ -19,18 +20,28 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		numOfDirections = 0;
+
+		// movement logic
 		if (Input.GetKeyDown("left shift")) {
 			shift = true;
 		}
 		if (Input.GetKey("up")) {
+			numOfDirections++;
 			move(dir.UP);
 		}
-		else if (Input.GetKey("down")) move(dir.DOWN);
-		if (Input.GetKey("left")) move(dir.LEFT);
-		else if (Input.GetKey("right")) move(dir.RIGHT);
-
-
-		// move();
+		else if (Input.GetKey("down")) {
+			numOfDirections++;
+			move(dir.DOWN);
+		}
+		if (Input.GetKey("left")) {
+			numOfDirections++;
+			move(dir.LEFT);
+		}
+		else if (Input.GetKey("right")) {
+			numOfDirections++;
+			move(dir.RIGHT);
+		}
 
 	}
 
@@ -38,11 +49,46 @@ public class Player : MonoBehaviour {
 
 	}
 
+	void canMove(){
+		//checking if outside bounds
+		if(transform.position.x >= 500) {
+
+			transform.position = new Vector2(500, transform.position.y);
+
+			Debug.Log("edge");
+			return;
+		}
+		else if(transform.position.x <= 0) {
+
+			transform.position = new Vector2(0, transform.position.y);
+
+			Debug.Log("edge");
+			return;
+		}
+		if(transform.position.x >= 500) {
+
+			transform.position = new Vector2(transform.position.x, 500);
+
+			Debug.Log("edge");
+			return;
+		}
+		else if(transform.position.x <= 0) {
+
+			transform.position = new Vector2(transform.position.x, 0);
+
+			Debug.Log("edge");
+			return;
+		}
+	}
+
 	void move(dir direction) {
 
 		if(shift) speed = 2;
 		else speed = 4;
+		if(numOfDirections==2) speed * Math.Sqrt(2);
 
+
+		//moving
 		if(direction == dir.UP) {
 			direction_vector = new Vector2(0, 1);
 			transform.Translate(direction_vector*speed*Time.deltaTime);
