67858875a2788333a9eaa63966d608bf5000ea38
Kevin Frans <kevin@bobthechicken.com>
2017-05-21 09:37:49 -0700

    gui

--- Assets/Scripts/God.cs
+++ Assets/Scripts/God.cs
@@ -6,9 +6,13 @@ public class God : MonoBehaviour {
 
 	// Prefab references
 	public Transform player;
-
 	public Transform boss1;
 	public Transform boss2;
+
+	//GUI references
+	public Transform score_text;
+	public Transform graze_text;
+	public Transform life_text;
 
 	// Use this for initialization
 	void Start () {
