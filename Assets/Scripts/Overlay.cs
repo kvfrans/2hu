577330c1e263b75ac9c3eb127c2ffef68c4325b8
Daniel Chu <dchu0518@gmail.com>
2017-05-24 09:07:41 -0700

    1 more try for overlay

--- Assets/Scripts/Overlay.cs
+++ Assets/Scripts/Overlay.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Overlay : MonoBehaviour {
 
+	public enum characters { REIMU, MARISA, PATCHOULI, ALICE };
+
+	private Vector2 direction_vector = new Vector2(1, 2);
+	private float speed = 6.0f;
+	private float time = 0;
+
 	public GameObject mainCamera;
 	public GameObject camera2;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +23,32 @@ public class Overlay : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		move();
+		updateSpeed();
+		
+
+	}
+
+	void move () {
+
+		time += Time.deltaTime;
+		transform.Translate(direction_vector*speed*Time.deltaTime*Helper.Instance.gameSpeed);
+
+	// // the syntax is GameObject.GetComponent<Overlay> ().changeCamera();
+	// void changeCamera () {
+
+	// 	mainCamera.GetComponent<Camera>().active = !mainCamera.GetComponent<Camera>().active;
+ //    	camera2.GetComponent<Camera>().active = !camera2.GetComponent<Camera>().active;
 
 
 	}
 
 
+	void updateSpeed () {
+
+		speed = (float) (Mathf.Pow(time-3, 2)/20 + 0.5);
+
+	}
 	// // the syntax is GameObject.GetComponent<Overlay> ().changeCamera();
 	// void changeCamera () {
 
@@ -32,6 +61,30 @@ public class Overlay : MonoBehaviour {
 	// GameObject checkCamera () {
 	// 	if(mainCamera.GetComponent<Camera>().active) return mainCamera;
 	// 	else return camera2;
+	// }
+
+	// // returns the camera that is currently active
+	// GameObject checkCamera () {
+	// 	if(mainCamera.GetComponent<Camera>().active) return mainCamera;
+	// 	else return camera2;
+	// }
+
+
+
+	// // the syntax is GameObject.GetComponent<Overlay> ().changeCamera();
+	// void changeCamera () {
+
+	// 	mainCamera.GetComponent<Camera>().active = !mainCamera.GetComponent<Camera>().active;
+ 	//   	camera2.GetComponent<Camera>().active = !camera2.GetComponent<Camera>().active;
+
+	// }
+
+	// // returns the camera that is currently active
+	// GameObject checkCamera () {
+
+	// 	if(mainCamera.GetComponent<Camera>().active) return mainCamera;
+	// 	else return camera2;
+
 	// }
 
 }
